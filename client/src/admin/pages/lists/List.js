7b5b03934485e906f14f0be60bfd0a598666ab2d
Phillip8232 <phillip8232@hotmail.com>
2019-12-09 11:04:06 +1100

    i've completed the login page and now i am working on navbar for partials

---
+++ client/src/admin/pages/lists/List.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../partials/Admin.css'
+import Navbar from '../partials/_navbar'
+export default function List() { //exporting this component into react app
+    return (
+        <div>
+            <Navbar />
+        </div>
+    );
+  }
+  
+  
