6368c2ebdc39bdd822b7aa901c2ea64413732327
Kats <katsu.0323.jpn@icloud.com>
2019-12-09 12:09:20 +1100

    show all routes properly

--- client/src/pages/price/Price.js
+++ client/src/pages/price/Price.js
@@ -1,11 +1,21 @@
-import React, { Component } from "./node_modules/react";
+import React, { Component } from "react";
 
 class Contact extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            title: "Hair Cut",
+            price: "$20"
+
+        }
+    }
     render() {
         return (
-
-            <h1>Price</h1>
-
+            <div>
+                <h1>Price</h1>
+                <h2>{this.state.title}</h2>
+                <h2>{this.state.price}</h2>
+            </div>
         );
     };
 };
