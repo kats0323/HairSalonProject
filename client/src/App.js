611c68f3500a7fc2300e19ec910d3f843ac8f433
Phillip8232 <phillip8232@hotmail.com>
2019-12-09 14:27:30 +1100

    made admin a seperate component

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,13 @@
 import React, { Component } from "react";
 import Navbar from "./pages/partials/_navbar" // Maybe give it a different name instead of navbar
-import Login from './admin/login/Login' //importing this from admin/login/Login.js
-import List from './admin/pages/lists/List' //importing this from admin/pages/list/list this is the admin page to check customers
-
-import { BrowserRouter as Router , Switch , Route } from "react-router-dom";
-
+import Admin from "./admin/pages/partials/_admin"
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Router>
-            <Switch>
-              <Route path="/" component={Navbar} / >
-              <Route path="/login" component={Login} />
-              <Route path="/admin" component={List} />
-            </Switch>
-        </Router>
+        <Navbar />
+        <Admin />
       </div>
     );
   };
