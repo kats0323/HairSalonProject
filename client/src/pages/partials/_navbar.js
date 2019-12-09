ceaea5fa117e6e2189999bba265a623cf0fcdd6e
Kats <katsu.0323.jpn@icloud.com>
2019-12-09 12:09:20 +1100

    show all routes properly

--- client/src/pages/partials/_navbar.js
+++ client/src/pages/partials/_navbar.js
@@ -1,6 +1,10 @@
 import React from "react";
-import { BrowserRouter, Link } from "react-router-dom";
-
+import { BrowserRouter, Link, Route } from "react-router-dom";
+import About from "../about/About";
+import PhotoGallery from "../photoGallery/PhotoGallery";
+import Contact from "../contact/Contact";
+import Price from "../price/Price";
+import TopPage from "../toppage/TopPage";
 
 
 function Navbar() {
@@ -14,6 +18,11 @@ function Navbar() {
                 <Link to="/contact">Contact</Link>
                 <Link to="/price">Price</Link>
                 <Link to="/photos">PhotoGallery</Link>
+                <Route exact path="/" component={TopPage} />
+                <Route exact path="/about" component={About} />
+                <Route exact path="/contact" component={Contact} />
+                <Route exact path="/price" component={Price} />
+                <Route exact path="/photos" component={PhotoGallery} />
             </div>
         </BrowserRouter >
 
