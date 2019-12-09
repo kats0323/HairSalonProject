7f840f195c3c06e2c14cbdfa15f77fbe8de7d644
Phillip8232 <phillip8232@hotmail.com>
2019-12-09 12:14:59 +1100

    i added comments in app.js and i also added the component navbar in

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Navbar from "./pages/partials/_navbar"
+import Navbar from "./pages/partials/_navbar" // Maybe give it a different name instead of navbar
 import Login from './admin/login/Login' //importing this from admin/login/Login.js
 import List from './admin/pages/lists/List' //importing this from admin/pages/list/list this is the admin page to check customers
 
@@ -9,15 +9,15 @@ import { BrowserRouter as Router , Switch , Route } from "react-router-dom";
 class App extends Component {
   render() {
     return (
-    <div>
-      <Router>
-          <Switch>
-            <Route path="/login" Component={Login} />
-            <Route path="/admin" Component={List} />
-          </Switch>
-      </Router>
-    </div>
-
+      <div>
+        <Router>
+            <Switch>
+              <Route path="/" component={Navbar} / >
+              <Route path="/login" component={Login} />
+              <Route path="/admin" component={List} />
+            </Switch>
+        </Router>
+      </div>
     );
   };
 };
