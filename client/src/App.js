e23b2237b05e9b592a844b5d1d4d3be698ad421e
Phillip8232 <phillip8232@hotmail.com>
2019-12-09 11:42:20 +1100

    added browserRouter into our app.js file

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,23 @@
 import React, { Component } from "react";
 import Navbar from "./pages/partials/_navbar"
+import Login from './admin/login/Login' //importing this from admin/login/Login.js
+import List from './admin/pages/lists/List' //importing this from admin/pages/list/list this is the admin page to check customers
+
+import { BrowserRouter as Router , Switch , Route } from "react-router-dom";
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar />
-      </div>
+    <div>
+      <Router>
+          <Switch>
+            <Route path="/login" Component={Login} />
+            <Route path="/admin" Component={List} />
+          </Switch>
+      </Router>
+    </div>
+
     );
   };
 };
