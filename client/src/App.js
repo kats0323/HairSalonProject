0005e00c1f5950443629ce65b5ca3a4d59b96146
Phillip8232 <phillip8232@hotmail.com>
2019-12-09 15:48:43 +1100

    fixed the router problem

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
-import Navbar from "./pages/partials/_navbar" // Maybe give it a different name instead of navbar
+import Overall from "./pages/partials/_overall" // Maybe give it a different name instead of navbar
 import Admin from "./admin/pages/partials/_admin"
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar />
-        <Admin />
-      </div>
+        <div>
+            <Overall />
+            <Admin />
+        </div>
     );
   };
 };
