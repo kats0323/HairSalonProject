2eb585a49cec54ff6b0bf93a8344e55b3c191996
Phillip8232 <phillip8232@hotmail.com>
2019-12-09 11:04:06 +1100

    i've completed the login page and now i am working on navbar for partials

---
+++ client/src/admin/login/Login.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Login.css' //importing css file from Login.css
+export default function Login() { //exporting this component into react app
+    return (
+        <div>
+            {/* this is the is the background with gradient and blurred effect */}
+            <div className="outerbox" style={{width:"100vw",height:"100vh"}} /> 
+            <div class="bg-text">
+            <h1>Login</h1>
+                <form>
+                    <input type="text" placeholder="Username" /> {/* Text field input */}
+                    <input type="password" placeholder="Password" />{/* Password field hides the password */}
+                    <button type="submit" id="login-button">Login</button>{/* this is the login button */}
+                </form>
+            </div>
+        </div>
+    );
+  }
+  
+  
