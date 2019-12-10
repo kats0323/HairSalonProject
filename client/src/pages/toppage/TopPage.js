4cad3fe6557b4ab9b2e7220addb7583cfdbea896
Kats <katsu.0323.jpn@icloud.com>
2019-12-10 11:04:47 +1100

    for recording

--- client/src/pages/toppage/TopPage.js
+++ client/src/pages/toppage/TopPage.js
@@ -1,14 +1,8 @@
 import React, { Component } from "react";
-<<<<<<< HEAD
 import 'antd/dist/antd.css';
 import { Carousel } from "antd";
 import '../toppage/top.style.css';
 
-=======
-import 'antd/dist/antd.css'; 
-import './carousel.css'; 
-import { Carousel } from 'antd';
->>>>>>> ad5e8091f6469d5f41006bfd237845dc6c1e9215
 
 
 
@@ -31,10 +25,5 @@ class TopPage extends Component {
             </Carousel>
         );
     };
-<<<<<<< HEAD
 }
-=======
-};
-
->>>>>>> ad5e8091f6469d5f41006bfd237845dc6c1e9215
 export default TopPage;
