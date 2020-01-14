9b6c721b96364ee4f0929aa3ed3e5d05ad078697
Phillip8232 <phillip8232@hotmail.com>
2020-01-15 09:51:44 +1100

    working on home page

--- client/src/pages/partials/_navbar.js
+++ client/src/pages/partials/_navbar.js
@@ -48,7 +48,7 @@ class Navbar extends Component {
                         {this.state.language === "Japanese" ? (
                             <Menu.Item key="1" className="english_button" style={{ color: "black" }} onClick={this.onChangeEnglish.bind(this)}>English</Menu.Item>
                         ) : (
-                                <Menu.Item key="1" className="japanese_button" style={{ color: "white" }} onClick={this.onChangeJapanese.bind(this)}>Japanese</Menu.Item>
+                                <Menu.Item key="1" className="japanese_button" style={{ color: "black" }} onClick={this.onChangeJapanese.bind(this)}>Japanese</Menu.Item>
                             )}
                         <Menu.Item key="5" > <Link to={{ pathname: "/" }}>Home</Link></Menu.Item>
                         <Menu.Item key="6" className="nav_bar_nest"> <Link to="/about">About</Link></Menu.Item>
--- client/src/pages/toppage/TopPage.js
+++ client/src/pages/toppage/TopPage.js
@@ -9,21 +9,34 @@ import { Carousel } from 'antd';
 
 class TopPage extends Component {
     render() {
+        
+        //  this is the hr line 
+         const Line = ({ color }) => (
+            <hr style={{ color: "gray",border:"0.5px solid", width:"80%",}} /> 
+         );
         return (
-            <Carousel autoplay>
-                <div class="Carousel-box">
-                    <img class="Carousel-photo" src={process.env.PUBLIC_URL + '/img/hair1.jpg'} alt="img1" />
+            <div>
+                <Carousel autoplay>
+                    <div class="Carousel-box">
+                        <img class="Carousel-photo" src={process.env.PUBLIC_URL + '/img/hair1.jpg'} alt="img1" />
+                    </div>
+                    <div class="Carousel-box">
+                        <img class="Carousel-photo" src={process.env.PUBLIC_URL + '/img/hair2.jpg'} alt="img2" />
+                    </div>
+                    <div class="Carousel-box">
+                        <img class="Carousel-photo" src={process.env.PUBLIC_URL + '/img/hair3.jpg'} alt="img3" />
+                    </div>
+                    <div class="Carousel-box">
+                        <img class="Carousel-photo" src={process.env.PUBLIC_URL + '/img/Caro1.png'} alt="img4" />
+                    </div>
+                </Carousel>
+
+                <div class="style-home">
+                    <h2>Style</h2>
+                    <Line />
                 </div>
-                <div class="Carousel-box">
-                    <img class="Carousel-photo" src={process.env.PUBLIC_URL + '/img/hair2.jpg'} alt="img2" />
-                </div>
-                <div class="Carousel-box">
-                    <img class="Carousel-photo" src={process.env.PUBLIC_URL + '/img/hair3.jpg'} alt="img3" />
-                </div>
-                <div class="Carousel-box">
-                    <img class="Carousel-photo" src={process.env.PUBLIC_URL + '/img/Caro1.png'} alt="img4" />
-                </div>
-            </Carousel>
+            </div>
+
         );
     };
 }
