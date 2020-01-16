7d8a25c71d979359baf84a8300c9976a6573989f
shun8765 <beachsk201706@icloud.com>
2020-01-16 22:52:20 +1100

    commit

--- client/src/admin/pages/partials/_navbar.js
+++ client/src/admin/pages/partials/_navbar.js
@@ -9,17 +9,19 @@ function Navbar() { //exporting this component into react app
     return (
         <div>
             <Router >
-                <aside>
-                    <div class="title-box">
+                {/* <aside> */}
+                    {/* <div class="title-box">
                         <h1 class="admin-title">Admin Page</h1>
-                    </div>
+                    </div> */}
+                    <div id="mydrawer">
                     <ul>
                         <div class="linktitle"><Link to="List" class="Link"><li>Customer List</li></Link></div>
                         <div class="linktitle"><Link to="/admin/alledit" class="Link"><li>Edit Pages</li></Link></div>
                         <div class="linktitle"><Link to="test2" class="Link"><li>test 2</li></Link></div>
                         <div class="linktitle"><Link to="test3" class="Link"><li>test 3</li></Link></div>
                     </ul>
-                </aside>
+                    </div>
+                {/* </aside> */}
                 <Switch> 
                     <Route exact path="/admin/alledit" component={AllEditPagesNavbar}></Route>
                 </Switch>
