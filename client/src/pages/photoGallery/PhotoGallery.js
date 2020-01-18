610dfbab9ac2ed891c29ee5888e022d06c525065
Kats <katsu.0323.jpn@icloud.com>
2020-01-18 14:38:13 +1100

    created modal images feature

--- client/src/pages/photoGallery/PhotoGallery.js
+++ client/src/pages/photoGallery/PhotoGallery.js
@@ -7,9 +7,16 @@ import './gallery.css'
 
 
 const PhotoFunction = props => (
+
     <tr>
-        <img src={props.photos.photo} style={{ width: "400px", margin: "20px", height: "300px" }} />
-    </tr>
+
+        <img src={props.photos.photo} style={{ width: "400px", margin: "20px", height: "300px", cursor: "pointer" }} class="photos_pg"
+            onClick={
+                props.popup_open
+            } />
+
+    </tr >
+
 )
 
 
@@ -31,11 +38,22 @@ export default class PhotoGallery extends Component {
             })
     }
 
+    popup_open(e) {
+        let popupContent = document.querySelector(".popup-content");
+        let popup_wrapper = document.querySelector(".popup-wrapper");
+        let photoData = e.target.src
+        console.log(photoData)
+        popupContent.innerHTML = '<img src=' + photoData + ' >'
+        popup_wrapper.style.display = "block"
+    }
 
-
+    popupClose() {
+        let popup_wrapper = document.querySelector(".popup-wrapper");
+        popup_wrapper.style.display = "none"
+    }
     photoList() {
         return this.state.photos.map(currentphoto => {
-            return <PhotoFunction photos={currentphoto} key={currentphoto._id} />;
+            return <PhotoFunction photos={currentphoto} key={currentphoto._id} popup_open={this.popup_open} />;
         })
     }
 
@@ -46,14 +64,22 @@ export default class PhotoGallery extends Component {
 
 
         return (
-            <div>
-                <h1 class="title-gallery-center">PhotoGallery</h1>
-                <ColoredLine />
-                <Row type="flex" justify="start">
-                    {this.photoList()}
-                    {this.photoList()}
-                    {this.photoList()}
-                </Row>
+            <div id="popup" >
+                <div id="popup_background">
+                    <div>
+                        <h1 class="title-gallery-center">PhotoGallery</h1>
+                        <ColoredLine />
+                        <Row type="flex" justify="start" >
+                            {this.photoList()}
+                        </Row>
+                        <div class="popup-wrapper">
+                            <div class="popup">
+                                <div class="popup-close" onClick={this.popupClose}>X</div>
+                                <div class="popup-content"  ></div>
+                            </div>
+                        </div>
+                    </div>
+                </div >
             </div>
         )
     }
